bdeb9b19316b14080d41fd50f8355ee2155c5723
sugoii77-rgb <sugoii77@gmail.com>
2025-08-28 11:53:42 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/KEDEHUMMap/",   // 반드시 레포 이름과 동일하게!
-});
+export default {
+  base: "./",   // <- /KEDEHUMMap/ 대신 이걸로
+};
